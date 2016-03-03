bff6c8b1a414804a9eca732cecd0ba3a45f3886f
Clément Fischer <clement.fischer@outlook.com>
2016-03-03 02:57:53 +0100

    Loaded meteo data. Gives the number of departements were it has rained, and the number of departements where the temperature went below 0. Everyday.

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -4,6 +4,7 @@ reads train data and exports filtered data into the output file
 
 import csv
 import pandas as pd
+import numpy as np
 from configuration import CONFIG
 
 
@@ -31,24 +32,61 @@ def parse_train_as_df(in_path, out_path, useful_cols=None, remove_days_off=True)
     df.to_csv(out_path)
 
 
+# def parse_meteo_as_df(in_path, out_path=None, useful_cols=None):
+#     """
+#     Parsing meteo file
+#     """
+#     # date_format='%Y-%m-%d %H:%M:%S.%f'
+#     if useful_cols is None:
+#         useful_cols = ['DATE', 'MIN_TEMP', 'PRECIP']  # date, temperature_min, precipitations
+#     header_row = ['DATE', 'DEPT', 'CITY', 'MIN_TEMP', 'MAX_TEMP', 'WIND_DIR', 'PRECIP', 'HPA']
+#     # because the meteo file index is missing  :(
+#     df = pd.read_csv(in_path,
+#                      sep=",",
+#                      usecols=useful_cols,
+#                      parse_dates=[0],
+#                      names=header_row)
+#
+#     grouped = df.groupby('DATE')  # group by date
+#     df = grouped["MIN_TEMP", 'PRECIP'].mean().reset_index()
+#     df[["MIN_TEMP", 'PRECIP']] = df[["MIN_TEMP", "PRECIP"]].astype(float)
+#     if out_path is not None:
+#         df.to_csv(out_path)
+#     return df
+
+
 def parse_meteo_as_df(in_path, out_path=None, useful_cols=None):
     """
     Parsing meteo file
     """
-    # date_format='%Y-%m-%d %H:%M:%S.%f'
+    # date_format='%Y-%m-%d %H:%M'
+    # print('Reading meteo...')
     if useful_cols is None:
-        useful_cols = ['DATE', 'MIN_TEMP', 'PRECIP']  # date, temperature_min, precipitations
+        useful_cols = ['DATE', 'DEPT', 'MIN_TEMP', 'PRECIP']  # date, temperature_min, precipitations
+    # if useful_cols is None:
+    #     useful_cols = [0, 1, 3, 6]  # date, temperature_min, precipitations
     header_row = ['DATE', 'DEPT', 'CITY', 'MIN_TEMP', 'MAX_TEMP', 'WIND_DIR', 'PRECIP', 'HPA']
     # because the meteo file index is missing  :(
     df = pd.read_csv(in_path,
                      sep=",",
                      usecols=useful_cols,
                      parse_dates=[0],
+                     date_parser=lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M'),
                      names=header_row)
-    
-    grouped = df.groupby('DATE')  # group by date
-    df = grouped["MIN_TEMP", 'PRECIP'].mean().reset_index()
-    df[["MIN_TEMP", 'PRECIP']] = df[["MIN_TEMP", "PRECIP"]].astype(float)
+    # print('Done.')
+
+    # print('Removing departments without digits...')
+    df=df[df['DEPT'].str.isdigit()]
+    # print('Done.')
+
+    # print('Removing departments equal to 00...')
+    df=df[df['DEPT']!='00']
+    # print('Done.')
+
+
+    # grouped = df.groupby('DATE')  # group by date
+    # df = grouped["MIN_TEMP", 'PRECIP'].mean().reset_index()
+    # df[["MIN_TEMP", 'PRECIP']] = df[["MIN_TEMP", "PRECIP"]].astype(float)
     if out_path is not None:
         df.to_csv(out_path)
     return df
@@ -84,9 +122,22 @@ def run(train_or_meteo=None):
     if train_or_meteo is None or train_or_meteo == 'train':
         parse_train_as_df(CONFIG.raw_train_path, CONFIG.preprocessed_train_path)
     if train_or_meteo is None or train_or_meteo == 'meteo':
+        print('Reading meteo file 1...')
         df1 = parse_meteo_as_df(CONFIG.raw_meteo_path1)
+        print('Reading meteo file 2...')
         df2 = parse_meteo_as_df(CONFIG.raw_meteo_path2)
-        pd.concat([df1, df2]).to_csv(CONFIG.preprocessed_meteo_path)
+        df = pd.concat([df1, df2])
+        print('Meteo dataframes concatenated.')
+        df['DATE']=df.DATE.map(lambda x: x.date())
+        print('Dates formated.')
+        grouped=df.groupby(['DATE','DEPT'])
+        df1=grouped.agg({'MIN_TEMP':np.min,'PRECIP':np.max}).reset_index()
+        df2=df1.groupby('DATE').agg({'MIN_TEMP':lambda x: pd.Series([(x <= 0).sum()]),'PRECIP':lambda x: pd.Series([(x > 0).sum()])})
+        df2=df2.rename(columns = {'MIN_TEMP':'NUMB_FROZEN_DEPT'})
+        df2=df2.rename(columns = {'PRECIP':'NUMB_WET_DEPT'})
+        df2.to_csv(CONFIG.preprocessed_meteo_path)
+        print('Done.')
+
 
 
 if __name__ == "__main__":
